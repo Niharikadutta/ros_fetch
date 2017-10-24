392cebb7aa14e189fee8bde1dd0ae23f3e813cf0
bewilderon <niharika.dutta91@gmail.com>
2017-10-23 22:51:09 -0700

    First push of code to take input of poses from user and execute on Fetch

---
+++ build/fetch_disco_moveit/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/bewilderon/catkin_ws/src/fetch_disco_moveit
+# Build directory: /home/bewilderon/catkin_ws/build/fetch_disco_moveit
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
