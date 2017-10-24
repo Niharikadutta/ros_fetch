2e3a88558478f9240116b675c633de9e525c2d7c
bewilderon <niharika.dutta91@gmail.com>
2017-10-23 23:05:58 -0700

    Removed irrelevant packages

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,4 +7,8 @@
 # new environment variables
 
 # modified environment variables
+export CPATH="/opt/ros/indigo/include"
+export LD_LIBRARY_PATH="/opt/ros/indigo/lib:/opt/ros/indigo/lib/x86_64-linux-gnu"
+export PKG_CONFIG_PATH="/opt/ros/indigo/lib/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig"
 export PWD="/home/bewilderon/catkin_ws/build"
+export PYTHONPATH="/opt/ros/indigo/lib/python2.7/dist-packages"
